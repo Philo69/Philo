0e9bcb8770bbbabb3c3bb3b71b6dbb5774b57737
Philo69 <120897603+Philo69@users.noreply.github.com>
2024-09-29 08:43:26 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,16 +2,16 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
-    PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
+    OWNER_ID = "7202072688"
+    sudo_users = "7222795580", "6180999156", "7202072688",
+    GROUP_ID = -1001548130580
+    TOKEN = "6862816736:AAEpfKqiIbeajsUyoqofBhtmayY1HAzjeYY"
+    mongo_url = "mongodb+srv://pythoncux:pythoncux@cluster0.tl7krxg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    PHOTO_URL = ["https://telegra.ph/file/8d9c4af9b7705b1aafc99.jpg", "https://telegra.ph/file/8d9c4af9b7705b1aafc99.jpg"]
+    SUPPORT_CHAT = "PhiloMusicSupport"
+    UPDATE_CHAT = "TechPiroBots"
+    BOT_USERNAME = "@Waifu_Anime_Catch_Bot"
+    CHARA_CHANNEL_ID = "-1002140495801"
     api_id = 26626068
     api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
 
